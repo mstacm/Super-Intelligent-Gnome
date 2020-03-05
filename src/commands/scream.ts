bbd33082c67da9847d0852c650038ca3e84d9144
Gavin <grncheetah10@gmail.com>
2020-03-05 15:29:06 -0600

    Added logging to scream

--- src/commands/scream.ts
+++ src/commands/scream.ts
@@ -1,5 +1,6 @@
 import { Client, Message } from "discord.js";
 import { sendCheckup, sendToChannel } from "../send";
+import { logBot } from "../logging_config";
 
 // Yell at everyone on every server. This will definitely make friends.
 
@@ -10,6 +11,7 @@ function cmdScream(message: Message, client: Client) {
   const title: string = message.content.substring(8, index);
   const toSend: string = message.content.substring(index + 1);
 
+  logBot.debug("Sending scream message.");
   sendCheckup(message, SERVERS, toSend, title, client);
 }
 
